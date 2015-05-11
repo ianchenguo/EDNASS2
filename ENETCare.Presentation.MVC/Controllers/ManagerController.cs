ab9a6e0550ac3779f570cc0d2c036c217634b881
eatjeff <dabeige@gmail.com>
2015-05-11 23:40:29 +1000

    Manager Page, Index modified

---
+++ ENETCare.Presentation.MVC/Controllers/ManagerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ENETCare.Presentation.MVC.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public ActionResult ManagerHome()
+        {
+            return View();
+        }
+
+        public ActionResult ManagerDistributionCentreStock()
+        {
+            return View();
+        }
+
+        public ActionResult ManagerGlobalStock()
+        {
+            return View();
+        }
+
+        public ActionResult ManagerDistributionCentreLosses()
+        {
+            return View();
+        }
+
+        public ActionResult ManagerValueInTransit()
+        {
+            return View();
+        }
+
+        public ActionResult ManagerDoctorActivity()
+        {
+            return View();
+        }
+
+        public ActionResult ManagerDoctorActivityDoctors()
+        {
+            return View();
+        }
+    }
+}
