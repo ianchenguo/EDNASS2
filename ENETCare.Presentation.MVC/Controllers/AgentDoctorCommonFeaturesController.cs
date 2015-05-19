d9489e32679aa15cd746c3a3a68366c64a78f42b
kedongkai <Dongkai.Ke@student.uts.edu.au>
2015-05-19 22:36:09 +1000

    communication with business layer demo

--- ENETCare.Presentation.MVC/Controllers/AgentDoctorCommonFeaturesController.cs
+++ ENETCare.Presentation.MVC/Controllers/AgentDoctorCommonFeaturesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ENETCare.Business;
 
 namespace ENETCare.Presentation.MVC.Controllers
 {
@@ -19,9 +20,19 @@ namespace ENETCare.Presentation.MVC.Controllers
             return View();
         }
         // GET: AgentDoctorRegisterPackage
+        [HttpGet]
         public ActionResult AgentDoctorRegisterPackages()
         {
-            return View();
+            List<MedicationType> list = new MedicationTypeBLL().GetMedicationTypeList();
+            return View(list);
+        }
+        [HttpPost]
+        public ActionResult AgentDoctorRegisterPackages(int MedicationType)
+        {
+            List<MedicationType> list = new MedicationTypeBLL().GetMedicationTypeList();
+            string expireDate = "2015-12-31";
+            new MedicationPackageBLL("agent1@enetcare.com").RegisterPackage(MedicationType, expireDate);
+            return View(list);
         }
         // GET: AgentDoctorSendPackage
         public ActionResult AgentDoctorSendPackage()
