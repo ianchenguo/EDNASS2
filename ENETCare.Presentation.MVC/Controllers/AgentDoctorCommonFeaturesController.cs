e87da6a42cb08f43c3afb8bfa62d4e1214777cfa
11582145 Kelin Liao <11582145@uts.student.edu.au>
2015-05-25 11:40:00 +1000

    RegisterPageFunctional

--- ENETCare.Presentation.MVC/Controllers/AgentDoctorCommonFeaturesController.cs
+++ ENETCare.Presentation.MVC/Controllers/AgentDoctorCommonFeaturesController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using ENETCare.Business;
 using System.Data.Entity;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace ENETCare.Presentation.MVC.Controllers
 {
@@ -23,23 +24,62 @@ namespace ENETCare.Presentation.MVC.Controllers
         }
         // GET: AgentDoctorRegisterPackage
         //int? MediTypeId
+        //[HttpGet]
+        //public ActionResult AgentDoctorRegisterPackages(MedicationType MediTypeId)
+        //{
+        //    List<MedicationType> list = new MedicationTypeBLL().GetMedicationTypeList();
+        //    ViewData["day"] = "wocao viewdata";
+        //    ViewBag.inputValue = "wocao inputValue.";
+        //    ViewBag.dayD = ViewData["day"];
+        //    ViewData["dayDay"] = "nimabit3";
+        //    Debug.WriteLine("---ViewData---: " + ViewData["day"]);
+
+        //    MedicationType oneMedi = MediTypeId;
+        //    if(oneMedi.ID != 0)
+        //    {
+        //        MedicationType godMedi = new MedicationTypeBLL().GetMedicationTypeById(oneMedi.ID);
+        //        Debug.WriteLine("godMedi --- : " + godMedi.DefaultExpireDate.ToString());
+        //    }
+        //    else{
+        //        Debug.WriteLine("wocao nothing here.");
+        //    }
+
+
+        //    return View(list);
+        //}
         [HttpGet]
         public ActionResult AgentDoctorRegisterPackages()
         {
             List<MedicationType> list = new MedicationTypeBLL().GetMedicationTypeList();
+            //ViewData["strDay"] = DateTime.Now.ToString();
 
             return View(list);
         }
+
+
+
         [HttpPost]
-        public ActionResult AgentDoctorRegisterPackages(int MediTypeId)
+        public ActionResult AgentDoctorRegisterPackages(int MediTypeId, string AgentDoctorRegisterFormExpireDateInput)
         {
             List<MedicationType> list = new MedicationTypeBLL().GetMedicationTypeList();
+            
             MedicationType selectedMedi = new MedicationTypeBLL().GetMedicationTypeById(MediTypeId);
+            
             Debug.WriteLine("Name: -- " + selectedMedi.Name + "\n Default Expired Date: " + selectedMedi.DefaultExpireDate + "\n Value---: " + 
                 selectedMedi.Value + "\n ID is: " + selectedMedi.ID + "\n ShelfLife ---: " + selectedMedi.ShelfLife);
 
             string expireDate = "2015-12-31";
             new MedicationPackageBLL("agent1@enetcare.com").RegisterPackage(MediTypeId, expireDate);
+
+            if (AgentDoctorRegisterFormExpireDateInput != null && AgentDoctorRegisterFormExpireDateInput != "")
+            {
+                Debug.WriteLine("AgentDoctorRegisterFormExpireDateInput -------- " + AgentDoctorRegisterFormExpireDateInput);
+            }
+            else
+            {
+                Debug.WriteLine("Even no string.");
+            }
+
             return View(list);
         }
         // GET: AgentDoctorSendPackage
--- ENETCare.Presentation.MVC/Scripts/myJS/ad_Register.js
+++ ENETCare.Presentation.MVC/Scripts/myJS/ad_Register.js
@@ -1,7 +1,84 @@
-﻿jQuery(document).ready(function () {
+﻿$(document).ready(function () {
     AlertOny();
+    SelectedOptionValue();
 });
 
 function AlertOny() {
-    alert("I am an alert box!");
+    alert("AgentDoctor");
 };
+
+function SelectedOptionValue() {
+    var selectedList = $('select#AgentDoctorPackageRegisterPackageTypeDropDwonList');
+    // var selectedOpt = $('select#AgentDoctorPackageRegisterPackageTypeDropDwonList option:selected');
+    var sampleValue = $('div.agent-doctor-sample-selected-value');
+
+    selectedList.on('change', function () {
+        var hasValue = selectedList.has('[selected]');
+        var selectedOpt = $('select#AgentDoctorPackageRegisterPackageTypeDropDwonList option:selected');
+        var inputDisplay = $('#AgentDoctorRegisterFormExpireDateInput');
+
+        if (hasValue) {
+            // var selectedValueId = selectedOpt.val();
+            var seleOptExpDateElem = $('option.Expire-date-option-second');
+            seleOptExpDateElem.css('display', 'none');
+            var selectedOpVal = seleOptExpDateElem.val();
+            console.log('selectedOpVal ==== '+selectedOpVal);
+
+            //Id is string date, sample is "2017-06-24". 'yyyy-MM-dd'
+            var type = $.type(selectedOpVal);
+            // var expireDate333 = selectedOpVal;
+
+            var formatExpiredDate = formatDateFunc (selectedOpVal);
+            console.log('ed is '+formatExpiredDate);
+            inputDisplay.val(formatExpiredDate);
+
+            sampleValue.html('value is: ' + selectedOpVal);
+
+            // $.ajax({
+            //     type: "POST",
+            //     url: "GetTestJsonData",
+            //     data: selectedOpVal,
+            //     contentType: "application/json; charset=utf-8",
+            //     dataType: "json",
+            //     success: function (msg) {
+            //         alert("Name = " + msg.Name);
+            //     }
+            // });
+        } else {
+            // sampleValue.html('No Value  ------------------------div ');
+            console.log("wocao nothing.");
+        };
+    });
+
+    // var gan = $('.wocaoselected');
+
+}
+
+function formatDateFunc (optExpDate) {
+    var expireDate = optExpDate;
+    var typeD = $.type(expireDate);
+    console.log("passed expireDate: "+expireDate);
+    var n = expireDate.indexOf(" ");
+    var shortExpiredDate = expireDate.slice(0, n);
+    console.log('shortExpiredDate length'+shortExpiredDate.length);
+    var ExpiredDate1 = shortExpiredDate.replace('/', '-');
+    var n1 = ExpiredDate1.indexOf("-");
+    var n2 = ExpiredDate1.indexOf("/");
+    var Month = ExpiredDate1.slice(n1, n2);
+    console.log('Month is '+Month+" n1 "+n1+" n2 "+n2+" Length: "+Month.length);
+    if (Month.length<3 && Month.length > 0) {
+        console.log("Month[1] is: -- "+Month[1]);
+        var sigMonth = Month[1];
+        var completedMonth = "-0"+sigMonth;
+        console.log('completedMonth ==== '+completedMonth);
+        var eDate1 = ExpiredDate1.replace(Month, completedMonth);
+        console.log("eDate1 ==== "+eDate1);
+    };
+
+
+
+    var ExpiredDate2 = eDate1.replace('/', '-');
+    console.log('ExpiredDate2 is ==== '+ExpiredDate2);
+
+    return ExpiredDate2;
+}
