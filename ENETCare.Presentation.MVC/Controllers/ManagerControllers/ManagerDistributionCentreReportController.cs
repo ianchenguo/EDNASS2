30cd2b1d9f7c622b4c33978f08b0d9252778a5c6
ianchenguo <ianchenguo@gmail.com>
2015-05-26 08:17:20 +1000

    Manager home page and distribution centre report

---
+++ ENETCare.Presentation.MVC/Controllers/ManagerControllers/ManagerDistributionCentreReportController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ENETCare.Business;
+
+namespace ENETCare.Presentation.MVC.Controllers.ManagerControllers
+{
+    public class ManagerDistributionCentreReportController : Controller
+    {
+
+        private DistributionCentreBLL distributionCentresBLL;
+
+        public ManagerDistributionCentreReportController()
+        {
+            distributionCentresBLL = new DistributionCentreBLL();
+        }
+
+        // GET: ManagerDistributionCentreReport
+        public ActionResult Index()
+        {
+            return View(distributionCentresBLL.GetDistributionCentreList());
+        }
+
+
+    }
+}
---
+++ ENETCare.Presentation.MVC/Controllers/ManagerControllers/ManagerHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ENETCare.Presentation.MVC.Controllers.ManagerControllers
+{
+    public class ManagerHomeController : Controller
+    {
+        // GET: ManagerHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
