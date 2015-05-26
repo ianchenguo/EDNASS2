3775dd63a82889d3e6fc578b64da1490f863f51c
ianchenguo <ianchenguo@gmail.com>
2015-05-26 18:14:12 +1000

    Add back Manager views

---
+++ ENETCare.Presentation.MVC/Controllers/obselete/ManagerControllers/ManagerDistributionCentreLossesController.cs
@@ -0,0 +1,42 @@
+﻿using ENETCare.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ENETCare.Presentation.MVC.Controllers.ManagerControllers
+{
+    public class ManagerDistributionCentreLossesController : Controller
+    {
+        private DistributionCentreBLL distributionCentresBLL;
+        private ReportBLL reportBLL;
+
+        public ManagerDistributionCentreLossesController()
+        {
+            distributionCentresBLL = new DistributionCentreBLL();
+            reportBLL = new ReportBLL();
+        }
+
+        // GET: ManagerDistributionCentreReport
+        public ActionResult Index()
+        {
+            return View(distributionCentresBLL.GetDistributionCentreList());
+        }
+
+        public ActionResult Details(int id, string name)
+        {
+            var distributionCentreStock = reportBLL.DistributionCentreStock(id);
+
+            ViewBag.Name = name;
+            ViewBag.Sum = distributionCentreStock.Sum(c => c.Value);
+
+            if (distributionCentreStock == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(distributionCentreStock);
+        }
+    }
+}
---
+++ ENETCare.Presentation.MVC/Controllers/obselete/ManagerControllers/ManagerDistributionCentreReportController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ENETCare.Business;
+
+namespace ENETCare.Presentation.MVC.Controllers.ManagerControllers
+{
+    public class ManagerDistributionCentreReportController : Controller
+    {
+
+        private DistributionCentreBLL distributionCentresBLL;
+        private ReportBLL reportBLL;
+
+        public ManagerDistributionCentreReportController()
+        {
+            distributionCentresBLL = new DistributionCentreBLL();
+            reportBLL = new ReportBLL();
+        }
+
+        // GET: ManagerDistributionCentreReport
+        public ActionResult Index()
+        {
+            return View(distributionCentresBLL.GetDistributionCentreList());
+        }
+
+        public ActionResult Details(int id, string name)
+        {
+            var distributionCentreStock = reportBLL.DistributionCentreStock(id);
+
+            ViewBag.Name = name;
+            ViewBag.Sum = distributionCentreStock.Sum(c => c.Value);
+
+            if (distributionCentreStock == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(distributionCentreStock);
+        }
+    }
+}
