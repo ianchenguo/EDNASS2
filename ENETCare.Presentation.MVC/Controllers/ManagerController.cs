5d581851e1b248ab93a11951cf4faba6d020d7b2
ianchenguo <ianchenguo@gmail.com>
2015-05-26 18:10:14 +1000

    manager features: all pages withou no data

--- ENETCare.Presentation.MVC/Controllers/ManagerController.cs
+++ ENETCare.Presentation.MVC/Controllers/ManagerController.cs
@@ -1,49 +1,95 @@
-﻿using System;
+﻿using ENETCare.Business;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ENETCare.Business;
 
 namespace ENETCare.Presentation.MVC.Controllers
 {
     public class ManagerController : Controller
     {
-        // GET: Manager
-        public ActionResult ManagerHome()
+        private DistributionCentreBLL distributionCentresBLL;
+        private ReportBLL reportBLL;
+        private EmployeeBLL employeeBLL;
+
+        public ManagerController()
+        {
+            distributionCentresBLL = new DistributionCentreBLL();
+            reportBLL = new ReportBLL();
+            employeeBLL = new EmployeeBLL();
+        }
+
+        // GET: Home
+        public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult ManagerDistributionCentreStock()
+        // GET: CentreList
+        public ActionResult CentreList()
         {
-            return View();
+            return View(distributionCentresBLL.GetDistributionCentreList());
         }
 
-        public ActionResult ManagerGlobalStock()
+        // GET: DoctorList
+        public ActionResult DoctorList()
         {
-            List<MedicationTypeViewData> list = new ReportBLL().GlobalStock();
-            return View(list);
+            return View(employeeBLL.GetEmployeeListByRole(Role.Doctor));
         }
 
-        public ActionResult ManagerDistributionCentreLosses()
+        // GET: DistributionCentreStock
+        public ActionResult DistributionCentreStock(int id, string name)
         {
-            return View();
+            var distributionCentreStock = reportBLL.DistributionCentreStock(id);
+
+            ViewBag.Name = name;
+            ViewBag.Sum = distributionCentreStock.Sum(c => c.Value);
+
+            if (distributionCentreStock == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(distributionCentreStock);
         }
 
-        public ActionResult ManagerValueInTransit()
+        // GET: DoctorActivity
+        public ActionResult DoctorActivity(string name)
         {
-            return View();
+            var doctoryActivity = reportBLL.DoctorActivity(name);
+
+            ViewBag.Name = name;
+            ViewBag.Sum = doctoryActivity.Sum(c => c.Value);
+
+            if (doctoryActivity == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(doctoryActivity);
         }
 
-        public ActionResult ManagerDoctorActivity()
+        // GET: GlobalStock
+        public ActionResult GlobalStock()
         {
-            return View();
+            var globalStock = reportBLL.GlobalStock();
+            ViewBag.Sum = globalStock.Sum(c => c.Value);
+            return View(globalStock);
         }
 
-        public ActionResult ManagerDoctorActivityDoctors()
+        // GET: DistributionCentreLosses
+        public ActionResult DistributionCentreLosses()
         {
-            return View();
+            return View(reportBLL.DistributionCentreLosses());
+        }
+
+        // GET: ValueInTransit
+        public ActionResult ValueInTransit()
+        {
+            var valueInTransit = reportBLL.ValueInTransit();
+            ViewBag.Sum = valueInTransit.Sum(c => c.Value);
+            return View(valueInTransit);
         }
     }
 }
