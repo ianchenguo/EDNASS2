6a55c8e6baff2639d6c9db459e5aef94530670f5
11582145 Kelin Liao <11582145@uts.student.edu.au>
2015-05-31 23:12:30 +1000

    Separate Doctor Distribute

---
+++ ENETCare.Presentation.MVC/Controllers/DoctorDistributeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ENETCare.Business;
+using ENETCare.Presentation.MVC.Models;
+
+namespace ENETCare.Presentation.MVC.Controllers
+{
+    [Authorize(Roles = "Doctor")]
+
+    public class DoctorDistributeController : Controller
+    {
+        #region Properties
+
+        MedicationPackageBLL medicationPackageBLL;
+        MedicationPackageBLL MedicationPackageBLL
+        {
+            get
+            {
+                if (medicationPackageBLL == null)
+                {
+                    medicationPackageBLL = new MedicationPackageBLL(User.Identity.Name);
+                }
+                return medicationPackageBLL;
+            }
+        }
+
+        MedicationTypeBLL medicationTypeBLL;
+        MedicationTypeBLL MedicationTypeBLL
+        {
+            get
+            {
+                if (medicationTypeBLL == null)
+                {
+                    medicationTypeBLL = new MedicationTypeBLL();
+                }
+                return medicationTypeBLL;
+            }
+        }
+
+        DistributionCentreBLL distributionCentreBLL;
+        DistributionCentreBLL DistributionCentreBLL
+        {
+            get
+            {
+                if (distributionCentreBLL == null)
+                {
+                    distributionCentreBLL = new DistributionCentreBLL();
+                }
+                return distributionCentreBLL;
+            }
+        }
+
+        #endregion
+
+        #region
+        public ActionResult DoctorMasterPage()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Doctor Distribute Package
+
+        [HttpGet]
+        public ActionResult DoctorDistributePackage()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DoctorDistributePackage(string DoctorDistributePackageTypebarcode)
+        {
+            new MedicationPackageBLL(User.Identity.Name).DistributePackage(DoctorDistributePackageTypebarcode, true);
+            return View();
+        }
+
+        #endregion
+    }
+}
